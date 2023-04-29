228c52a78240668ea4fd0aef4714a735ba6229e3
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-04-29 15:46:00 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -14,3 +14,10 @@ class Setting():
 
         #настройки корабля
         self.ship_speed = 1.5
+
+        #параметр снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
