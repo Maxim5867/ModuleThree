2a73b39966e30df9fd0b75f3b8da4727afec75f4
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-04 18:46:06 +0300

    ДЗ

---
+++ setting1.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Setting():
+    screen_width = 800
+    screen_height = 600
+    bg_color = (230,230,230)
+    ship_speed = 1
+    bullets_allowed = 3
+    bullet_width = 15#потом заменить на 3
+    bullet_height = 3#потом заменить н6а 15
+    bullet_speed = -1
+    bullet_color = (60,60,60)
