8516af0f0ccf5c351422b9a7aff8347a78fb8a7c
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-06 14:08:26 +0300

    DZ1

--- alien_war1.py
+++ alien_war1.py
@@ -1,8 +1,8 @@
 import sys
 import pygame
-from setting import Setting
-from ship import Ship
-from bullet import Bullet
+from setting1 import Setting
+from ship1 import Ship
+from bullet1 import Bullet
 
 class AlienInvasion:
     #класс для управления русурсами и поведениями игр
@@ -14,13 +14,16 @@ class AlienInvasion:
         
 
         Vopr = input('''В каком режиме вы хотите играть:
-        в полноэкранном(G) или в оконном(O)''')
-        if Vopr == 'G':
+        в полноэкранном(1) или в оконном(0)''')
+        if Vopr == '1':
             self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
             self.settings.screen_width = self.screen.get_rect().width
             self.settings.screen_height = self.screen.get_rect().height
-        elif Vopr == 'O':
+        elif Vopr == '0':
             self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        else:
+            Vopr = input('''В каком режиме вы хотите играть:
+            в полноэкранном(1) или в оконном(0)''')
 
         pygame.display.set_caption('Инопланетное вторжение')
         
@@ -42,7 +45,7 @@ class AlienInvasion:
         self.bullets.update()
         #удаление снарядов, вышедших за край экрана
         for bullet in self.bullets.copy():
-            if bullet.rect.x > 1600:
+            if bullet.rect.x > 1400:
                 self.bullets.remove(bullet)
         
     def _check_event(self):
@@ -51,12 +54,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        #переместить корабль вправо
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        #переместить корабль влево
-                        self.ship.moving_left = True  
+                    if event.key == pygame.K_w:
+                        #переместить корабль наверх
+                        self.ship.moving_up = True
+                    elif event.key == pygame.K_s:
+                        #переместить корабль вниз
+                        self.ship.moving_down = True  
                     elif event.key == pygame.K_q:
                         sys.exit() 
                     elif event.key == pygame.K_SPACE:
@@ -68,12 +71,12 @@ class AlienInvasion:
                         #переместить корабль вниз
                         #self.ship.moving_under = True
                 elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        #конец перемещения корабля вправо
-                        self.ship.moving_right = False  
-                    if event.key == pygame.K_LEFT:
-                        #конец перемещения корабля вправо
-                        self.ship.moving_left = False 
+                    if event.key == pygame.K_w:
+                        #конец перемещения корабля наверх
+                        self.ship.moving_up = False  
+                    if event.key == pygame.K_s:
+                        #конец перемещения корабля вниз
+                        self.ship.moving_down = False 
                     #if event.key == pygame.K_DOWN:
                         #конец перемещения корабля наверх
                         #self.ship.moving_up = False
--- setting1.py
+++ setting1.py
@@ -7,6 +7,6 @@ class Setting():
     ship_speed = 1
     bullets_allowed = 3
     bullet_width = 15#потом заменить на 3
-    bullet_height = 3#потом заменить н6а 15
-    bullet_speed = -1
+    bullet_height = 3#потом заменить на 15
+    bullet_speed = -1.5
     bullet_color = (60,60,60)
