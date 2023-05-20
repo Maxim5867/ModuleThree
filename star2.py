3d42d189dcd3430f910f2a3d923d3f9943c73fe7
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-20 13:25:48 +0300

    DZ2

--- star2.py
+++ star2.py
@@ -5,7 +5,7 @@ class Star(Sprite):
     #Класс, представляющий одного пришельца
 
     def __init__(self,ai_game):
-        #инициализирует пришельца и задает его начальную позицию4
+        #инициализирует пришельца и задает его начальную позицию
         super().__init__()
         self.screen = ai_game.screen
 
@@ -17,5 +17,5 @@ class Star(Sprite):
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        #сохранение точной горизонтальной позиции пришельца 
+        #сохранение точной горизонтальной позиции пришельца
         self.x = float(self.rect.x)
