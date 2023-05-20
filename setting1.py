371384941a2980945b504b66ea79064d94cc4696
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-20 13:23:04 +0300

    DZ1

--- setting1.py
+++ setting1.py
@@ -1,12 +1,27 @@
 import pygame
 
 class Setting():
+    #Класс для хранения всех настроек игры
+
+    #инициализируем настройки игры
+
+    #параметры экрана
     screen_width = 800
     screen_height = 600
-    bg_color = (230,230,230)
-    ship_speed = 1
-    bullets_allowed = 3
-    bullet_width = 15#потом заменить на 3
-    bullet_height = 3#потом заменить на 15
-    bullet_speed = -1.5
+    bg_color = (250,250,250)
+
+    #настройки корабля
+    ship_speed = 1.5
+
+    #параметр снаряда
+    bullet_speed = -1
+    bullet_width = 15
+    bullet_height = 3
     bullet_color = (60,60,60)
+    bullets_allowed = 5
+
+    #настройки пришельцев
+    alien_speed = 0.2
+    fleet_drop_speed = 10
+    #fleet_direction = 1 - движение вправо, = -1 - движение влево
+    fleet_direction = 1
