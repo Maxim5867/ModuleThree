5f4c1a83a5d4661037ea5c28fe4fb1eea9d3bf17
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-20 13:22:45 +0300

    DZ1

--- bullet1.py
+++ bullet1.py
@@ -1,32 +1,27 @@
 import pygame
-import sys
 from pygame.sprite import Sprite
-
 
 class Bullet(Sprite):
     #класс для управления снарядами, выпущенными кораблем
 
     def __init__(self,ai_game):
-        #создать объект снарядов в текущем положении корабля
+        #слздать объект снарядов в текущем положении корабля
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
         #создание снаряда в позиции (0,0) и назначение правильной позиции
-        self.rect = pygame.Rect(0,10,self.settings.bullet_width,self.settings.bullet_height)
-        self.rect.midleft = ai_game.ship.rect.midright
+        self.rect = pygame.Rect(0,0,self.settings.bullet_width,self.settings.bullet_height)
+        self.rect.midbottom = ai_game.ship.rect.midright
+
         #позиция снаряда хранится в вещственном формате
         self.x = float(self.rect.x)
-        #self.bullet = Bullet(ai_game)
-        #angle = -10
-        #self.bullet = pygame.transform.rotate(angle,self.bullet)
     
     def update(self):
         #перемещает снаряд вверх по экрану
         #обновляет позиции снаряда в вещественном формате
         self.x -= self.settings.bullet_speed
-        
         #обновление позиции прямоугольника
         self.rect.x = self.x
     
