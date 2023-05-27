48e374a4ca1f324c58b491906818d893ec238835
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-05-27 15:56:54 +0300

    Work

--- button.py
+++ button.py
@@ -9,7 +9,7 @@ class Button():
 
         #назначим размеры и свойства кнопки
         self.width, self.height = 200,50
-        self.button_color = (0,255,0)
+        self.button_color = (139,0,0)
         self.text_color = (255,255,255)
         self.font = pygame.font.SysFont(None,48)
 
