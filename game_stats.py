50d85fc55f9c779cee773c9ff1ed692623bf589e
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-06-20 14:56:17 +0300

    DZ

--- game_stats.py
+++ game_stats.py
@@ -5,12 +5,18 @@ class GameStats():
         #получим настройки игры
         self.settings = ai_game.settings
         self.reset_stats()
-        self.high_score = 0
         #игра будет запускаться в активном состоянии
         self.game_active = False
+        with open("record.txt", "r") as rec_file:
+            score_file = rec_file.read()
+            if score_file != '':
+                self.high_score = int(score_file)
+            else:
+                self.high_score = 0
+
 
     def reset_stats(self):
         #инициализирую статистику, которая изменяется в процессе игры
-        self.ships_lifes = self.settings.ship_limit
+        self.ships_left = self.settings.ship_limit
         self.score = 0
         
